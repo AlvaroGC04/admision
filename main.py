dd7e3111be241b775485c0a750ce588e74ade777
AlvaroGalvis <79771552+AlvaroGC04@users.noreply.github.com>
2024-11-15 12:22:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,8 +4,9 @@ from code_challenge_two import square_result
 
 
 def main():
+    print("El numero hash correspondiente a Alvaro Galvis es 3")
     print("***Primer ejercicio***")
-    reverse_remove(6,[60, 6, 5, 4, 3, 2, 7, 7, 29, 1])
+    reverse_remove(3,[60, 6, 5, 4, 3, 2, 7, 7, 29, 1])
     print("-------------------------------------------")
     print("***Segundo ejercicio***")
     square_result(3, [-6, -5, 0, 5, 6])
